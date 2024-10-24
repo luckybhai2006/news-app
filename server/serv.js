f10074d046224d7d1d714152d80899e903eef4a0
luckybhai2006 <143918091+luckybhai2006@users.noreply.github.com>
2024-10-24 10:27:57 +0530

    Updated feature X

--- server/serv.js
+++ server/serv.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const app = express();
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
